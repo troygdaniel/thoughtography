a4a6a3902428697172b73f6044bff7564c5a899b
Troy Daniel <troy.daniel@mosaic.com>
2013-11-17 14:59:41 -0500

    Ow.  Grunt no like Meteor

--- leaderboard.js
+++ leaderboard.js
@@ -1,7 +1,6 @@
 // Set up a collection to contain player information. On the server,
 // it is backed by a MongoDB collection named "players".
-
-Players = new Meteor.Collection("players");
+var Players = new Meteor.Collection("players");
 
 if (Meteor.isClient) {
   Template.leaderboard.players = function () {

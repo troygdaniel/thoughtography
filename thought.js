aba421889f359d9f86e60e73cc658fdf60b17705
Troy Daniel <troy.daniel@mosaic.com>
2013-11-23 17:38:23 -0500

    UI refinements

--- thought.js
+++ thought.js
@@ -1,5 +1,16 @@
 $(function() {
+  $(".toggle-writer").click(function() {
+    var $leftpanel = $("#left-panel");
+    $leftpanel.toggle();
+    if ($leftpanel.is(":visible")) {
+      $(".toggle-writer").html("Hide writer");
+      $("#mid-panel").css("width","60%");
+    } else {
+      $(".toggle-writer").html("Show writer");
+      $("#mid-panel").css("width","100%");
+    }
 
+  });
   $.ajax("thought.json").done(function(json) {
     renderJsonAsTree(json);
   });

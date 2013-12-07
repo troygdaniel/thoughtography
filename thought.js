5e38ddb1b85d6a2c88a187893088d32c70007cd2
Troy Daniel <troy.daniel@mosaic.com>
2013-12-07 12:35:45 -0500

    UX refinements

--- thought.js
+++ thought.js
@@ -7,20 +7,31 @@ $(function() {
     noteId = location.search.substr(1,location.search.length);
   }
   $("#roomIdHeader").text(noteId);
-  
+  $("#view-link").click(function() { showRightPanel(); });
+  $("#create-link").click(function() { showLeftPanel(); });
+
   // Create a note model that binds to a websocket connection 
   note = new Take.Note({id: noteId,socket: socket});
   // Create a view that updates the DOM when receiving updates for a given note
   noteView = new Take.NoteView({note: note,el: "#topic-tree",socket: socket, textarea: ".braindump"});
 
   if (location.hash === "#creator") {
-    var $leftpanel = $("#left-panel");
-    $leftpanel.show();
+    showLeftPanel();
+  }
+
+  function showLeftPanel(){
+    $("#left-panel").show();
     $("#temp-content-reader").hide();
     $("#temp-content-creator").show();
-    $(".toggle-writer").html("Hide writer");
     $("#mid-panel").css("width", "40%");
     $("#mid-panel").css("font-size", "10px");
+  }
+  function showRightPanel() {
+    $("#left-panel").hide()    
+    $("#temp-content-reader").show();
+    $("#temp-content-creator").hide();
+  $("#mid-panel").css("width", "100%");
+  $("#mid-panel").css("font-size", "18px");
   }
 });
 

ec22af19449ae8c4fc22f793ddc5c84a347f9d17
Troy Daniel <troy.daniel@mosaic.com>
2013-12-08 22:34:03 -0500

    New Insights class to pull out wiki's, #tags and @people

--- jasmine/insights_spec.js
+++ jasmine/insights_spec.js
@@ -1,10 +1,9 @@
 //     Specs for Take.Insights
 describe("Take.Insights spec", function() {
-  window.noteContent = 'This is a test @daniel @troy @jeff.\n\tThis is a child with a wiki "hyperlink" #interwebs\nThis is another parent with a #hashtag topic\n\tThis is another child that @troy mentioned\n\t\tThis is a grandchild with @jeff\n';
+  var noteContent = 'This is a test @daniel @troy @jeff.\n\tThis is a child with a wiki "hyperlink" #interwebs\nThis is another parent with a #hashtag topic\n\tThis is another child that @troy will order lunch tomorrow.\n\t\tThis is a grandchild with @jeff\n';
   var socket = socket || io.connect('http://' + document.domain);
-  window.note = new Take.Note({id: 123,socket: socket});
-  window.insights = new Take.Insights({note: note});
-
+  var note = new Take.Note({id: 123,socket: socket});
+  window.insights = new Take.Insights({note: note});  
 
   describe("Constructor", function() {
     it("should have an instance of a Take.Insights", function() {
@@ -14,8 +13,7 @@ describe("Take.Insights spec", function() {
   
   describe("#sanitizedWords", function() {
     it("should return an array of words", function() {
-      var expectedArray = ["This", "is", "a", "test", "@daniel", "@troy", "@jeff", "", "", "This", "is", "a", "child", "with", "a", "wiki", "\"hyperlink\"", "#interwebs", "This", "is", "another", "parent", "with", "a", "#hashtag", "topic", "", "This", "is", "another", "child", "that", "@troy", "mentioned", "", "", "This", "is", "a", "grandchild", "with", "@jeff", ""];
-      console.log(insights.sanitizedWords(noteContent));
+      var expectedArray = ["This", "is", "a", "test", "@daniel", "@troy", "@jeff", "", "", "This", "is", "a", "child", "with", "a", "wiki", "\"hyperlink\"", "#interwebs", "This", "is", "another", "parent", "with", "a", "#hashtag", "topic", "", "This", "is", "another", "child", "that", "@troy", "will", "order", "lunch", "tomorrow", "", "", "", "This", "is", "a", "grandchild", "with", "@jeff", ""];
       expect(insights.sanitizedWords(noteContent)).toEqual(expectedArray);
     });
   });
@@ -52,36 +50,55 @@ describe("Take.Insights spec", function() {
     });
   });
 
+  describe("#wikiWord", function () {
+    it("should find the (single) wiki word from a given index", function() {
+      var noteTxt = 'This is a test example with wiki "medical science"\n\tThis is a child with a wiki "hyperlink" #interwebs\n';
+      var words = insights.sanitizedWords(noteTxt);
+      // wiki "hyperlink"
+      expect(insights.wikiWord(words,16)).toEqual("hyperlink");
+   });
+
+    it("should find the wiki words from a given index", function() {
+      var noteTxt = 'This is a test example with wiki "medical science"\n\tThis is a child with a wiki "hyperlink" #interwebs\n';
+      var words = insights.sanitizedWords(noteTxt);
+      // wiki "medical science"
+      expect(insights.wikiWord(words,6)).toEqual("medical_science");
+    });
+  });
+
   describe("#getWikis()", function() {
-    it("(pending...) should have array wiki links", function() {
+    it("should have array wiki links", function() {
+      var noteTxt = 'This is a test example with wiki "medical science"\n\tThis is a child with a wiki "hyperlink" #interwebs\n';
+      var href="http://en.m.wikipedia.org/w/index.php?title=";
+      var wikiArray = [{medical_science: href+"medical_science"},{hyperlink: href+"hyperlink"}];
+      note.setContent(noteTxt);
+      expect(insights.getWikis()).toEqual(wikiArray);
     });
   });
 
- describe("#getLinks", function() {
-    it("(pending...) should have array web links", function() {
+
+  describe("#actionItemSentence", function() {
+    it("should associate the line note with a participant", function() {
+      var words = insights.wordsArray(noteContent);
+      
+      expect(insights.actionItemSentence(words, 32)).toEqual("order lunch tomorrow.");
     });
   });
 
-  describe("#linesWithParticipant", function() {
-    it("(pending...) should associate the line note with a participant", function() {
-      // Annotate the JSON?
-      // return the lines?
+  describe("#actionItems", function() {
+    it("should associate the line note with a participant", function() {
+      var items = {"@troy": ["order lunch tomorrow."]};
+      var txt = noteContent;
+
+      note.setContent(txt);
+      expect(insights.actionItems()).toEqual(items);
+
+      txt += "and @daniel will ask @george if the requirements were correct."
+      note.setContent(txt);
+      
+      items["@daniel"] = ["ask @george if the requirements were correct."];
+      expect(insights.actionItems()).toEqual(items);      
     });
    });
-
-  describe("#actionItemsForParticipant", function() {
-    it("(pending...) should associate the line note with a participant", function() {
-      // Annotate the JSON?
-      // return the lines?
-    });
-   });
-
-  describe("#linesWithTag", function() {
-    it("(pending...) should have array of tags", function() {
-      // Annotate the JSON?
-      // return the lines?
-    });
-  });
-
 
 });
--- js/insights.js
+++ js/insights.js
@@ -2,7 +2,7 @@ var Take = Take || {};
 
 Take.Insights = function (options) {
   var note, _previousNote;
-  var participants = [], tags = [];
+  var participants = [], tags = [], wikis = [];
   var areParticipantsUpdated = false;
 
   initialize(options);
@@ -15,11 +15,45 @@ Take.Insights = function (options) {
   }
 
   function isNewParticipant(p) {
+    p = p.trim();
     return (p[0] === "@" && ($.inArray(p, participants) === -1))
   }
 
   function isNewTag(p) {
+    p = p.trim();
     return (p[0] === "#" && ($.inArray(p, tags) === -1))
+  }
+
+  function quoted(word) {
+    var firstChar = word[0];
+    var lastChar = word[word.length-1];    
+    return (firstChar === "\"" && lastChar === "\"");
+  }
+
+  function quoteless(word) {
+    return word.substr(1,word.length-2);
+  }
+
+  function nextTwoWords(words, i) {
+    var word = words[i];
+    return words[i].substr(1,word.length-1)+"_"+words[i+1].substr(0,word.length-1)
+  }
+
+  function addParticipant(v) {
+    participants.push(v.trim());
+  }
+
+  function addTag(v) {
+    tags.push(v.trim());
+  }
+
+  function wordsArray(v) {
+    var retVal = v;
+    if (retVal) {
+      retVal = retVal.split("\n").join(" ");
+      retVal = retVal.split("\t").join(" ");
+    }
+    return retVal.split(' ');;
   }
 
   // Public methods
@@ -35,35 +69,99 @@ Take.Insights = function (options) {
 
   function setNote(v){    
     _previousNote = note;
-    if (v != _previousNote) { setUpdateFlags(true); }
     note = v;
   }
+
   function getNote(){
     return note;
   }
 
   function getParticipants() {
-    
     var words = sanitizedWords(note.getContent());
-    for (var i = 0; i < words.length; i++) {
-      if (isNewParticipant(words[i].trim())) { participants.push(words[i].trim());  }
-    }
 
+    for (var i = 0; i < words.length; i++) {
+      var word = words[i];
+      if (isNewParticipant(word)) { 
+        addParticipant(word) 
+      }
+    }
     return participants;
   }
 
   function getTags() {
     var words = sanitizedWords(note.getContent());
     for (var i = 0; i < words.length; i++) {
-      if (isNewTag(words[i].trim())) { tags.push(words[i].trim());  }
+      var word = words[i];
+      if (isNewTag(word)) { 
+        addTag(word);  
+      }
     }
-
     return tags;    
+  }
+
+  function getWikis() {
+    var words = sanitizedWords(note.getContent());
+    var href="http://en.m.wikipedia.org/w/index.php?title=";
+    for (var i = 0; i < words.length; i++) {
+        if (words[i] === "wiki") {
+          var wikiHash = {};
+          wikiHash[wikiWord(words,i)] = href+wikiWord(words,i);
+          wikis.push(wikiHash);
+        }
+    }
+    return wikis; 
+  }
+
+  function wikiWord(words, startIndx) {
+    for (var i = startIndx+1; i < words.length; i++) {
+      var word = words[i];
+      
+      if (quoted(word) === true) { 
+        return quoteless(word);
+      } 
+      return nextTwoWords(words, i);
+    }
+  }
+
+  function actionItems() {
+    var words = wordsArray(note.getContent());
+    var items = {};
+
+    for (var i = 0; i < words.length; i++) {
+      var word = words[i];
+      var nextWord = words[i+1];
+      
+      if (word[0] === "@" && nextWord === "will") { 
+        if (typeof items[word] === "undefined") {
+          items[word] = [];
+        }
+        items[word].push(actionItemSentence(words, i+1));
+      }
+    }
+    return items;
+  }
+
+  function actionItemSentence(words, i) {
+    var actionString = "";
+    for (var indx = i+1; indx < words.length; indx++) {
+      var word = words[indx];
+      var lastChar = word[word.length-1];
+      actionString = actionString + word + " ";
+      if (lastChar === ".") {
+        break;
+      }
+    }
+    return actionString.trim();
   }
 
   return {
     getParticipants: getParticipants,
+    wordsArray: wordsArray,
     sanitizedWords: sanitizedWords,
+    actionItems: actionItems,
+    actionItemSentence: actionItemSentence,
+    getWikis: getWikis,
+    wikiWord: wikiWord,
     getNote: getNote,
     getTags: getTags
   };
